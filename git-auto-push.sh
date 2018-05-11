b6f512e057fa71eca20fa47ff58942e107d655f4
Xing Yong <xingyong@gmail.com>
2018-05-10 23:35:44 -0700

    info change

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-cd /home/xingyong/projects/public-info
+
+ENV="PROD"
+
+if [ "$ENV" = "PROD" ] ;then
+	cd /home/xingyong/projects/public-info
+	#echo $ENV
+	echo "prod env"
+elif [ "$ENV" = "DEV" ] ;then
+	cd /home/matrix/projects/public-info
+	#echo $ENV
+else
+	echo "unknown running env,exit."
+	exit -1
+fi
+
+echo "running..."
 git add .
 git commit -m 'info change'
 git push
